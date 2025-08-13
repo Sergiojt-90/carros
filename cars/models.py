08d7e9f936011864820e15e89b424b8dcca3a2c3
sergio <sergiobalboa176@gmail.com>
2025-08-13 16:08:25 -0300

    adicionado o template de base html

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,11 @@ class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+        ordering = ['name']
+
     def __str__(self):
         return self.name
     
@@ -18,6 +23,12 @@ class Car(models.Model):
     model_year = models.IntegerField(blank=True, null=True, verbose_name="Ano do Modelo")
     value = models.FloatField(blank=True, null=True, verbose_name="Valor")
     photo = models.ImageField(upload_to='cars/', blank=True, null=True, verbose_name="Foto")
+
+
+    class Meta:
+        verbose_name = "Carro"
+        verbose_name_plural = "Carros"
+        ordering = ['brand', 'model']
 
     def __str__(self):
         return self.model
