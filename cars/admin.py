f548b19a704d0629181b26324a051b572f4ee169
sergio <sergiobalboa176@gmail.com>
2025-08-13 10:14:53 -0300

    primeiro commit, ajuste na views de carros

---
+++ cars/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from cars.models import Car, Brand #, Model_Car
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id','model', 'brand', 'factory_year','model_year','value')
+    search_fields = ('model','brand')
+   
+
+class BrandAdmin(admin.ModelAdmin):
+   list_display = ('name',) 
+   search_fields = ('name',)
+
+'''class Model_CarAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)'''
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand,BrandAdmin)
+#admin.site.register(Model_Car, Model_CarAdmin)
---
+++ cars/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200, verbose_name="Modelo")
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand', verbose_name="Marca")
+    factory_year = models.IntegerField(verbose_name="Ano de Fabricação")
+    model_year = models.IntegerField(blank=True, null=True, verbose_name="Ano do Modelo")
+    value = models.FloatField(blank=True, null=True, verbose_name="Valor")
+
+
+    def __str__(self):
+        return self.model
+    
+
+
+    
+
+
+    
